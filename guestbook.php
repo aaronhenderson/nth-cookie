6dde54126446347044223040071714ba66c0e201
Aaron David Henderson <hello@aaronhenderson.co.uk>
2024-05-11 14:03:13 +0100

    Added some procedural gloatware..
    actually really difficult to write any code in this weather when being acosted, sanctioned and abused without proof but its possible. it's possible. don't give up on therapies and writing code to abstain from antisocial gatherings. sincerely and fraternally, me.

---
+++ guestbook.php
@@ -0,0 +1,40 @@
+<?php
+show_guest_book('maybe');
+
+
+// @joker:
+// @reference:
+// @reviewed-by:
+///////////////////////////////////////////////////////////////////////////////////////////
+
+function show_guest_book($include_document_nodes = false)                                 {
+?><!DOCTYPE html><?php
+///////////////////////////////////////////////////////////////////////////////////////////
+// -------------------------------------------------------------------- // 23/03/1990 â©µ //
+if($include_document_nodes === 'maybe'):
+?><html lang="en" xmlns="http://www.w3.org/1999/xhtml"><?php            // 11/05/2024 â©µ //
+?><head>
+    <title>lang::concatenated_english[] :: transactional_prometheus.php :: eoc ::  </title>
+</head>
+<body>
+<?php
+endif;
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+echo '<h2>Cyanide !â©µ  niacin..</h2>';
+//  php before the object model, view after the controller, peace before and after the war.
+echo '<pre>';
+$hits = file_get_contents('../logs/access.log');
+$hits = explode(PHP_EOL, $hits);
+$hits = array_reverse($hits);
+var_dump($hits);
+echo '</pre>';
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+// -------------------------------------------------------------------- // 08/05/2024 â©µ //
+if($include_document_nodes === 'maybe')                                                   {
+echo '</body>'; // // // // // // // //                                                   ~
+?></html><?php                                                                            }
+} /////////////////////////////////////////////////////////////////////////////////////////
